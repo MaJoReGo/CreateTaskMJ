db0cbe73869536c47e30db2fe227fe5c83a1820c
Maria Jose Restrepo <restrepogomezmariajose@gmail.com>
2024-03-05 13:10:22 -0500

    Creacion de carpeta CreateTask y su jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
 import './App.css'
+import { CreateTask } from './components/CreateTask/CreateTask'
+
 
 function App() {
   return (
     <>
+    <CreateTask />
     </> 
   )
 }
