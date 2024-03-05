de2aefcbbc3fd5ce25f851ed3dca4f70e9c69920
Maria Jose Restrepo <restrepogomezmariajose@gmail.com>
2024-03-05 13:56:32 -0500

    codigo funcional para agregar tareas

--- src/components/CreateTask/CreateTask.jsx
+++ src/components/CreateTask/CreateTask.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const CreateTask = () => {
-    return(
-    <>
-    </>
-    )
-}
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState('');
+
+    const handleInputChange = (event) => {
+        setNewTask(event.target.value);
+    };
+
+    const handleAddTask = () => {
+        if (newTask.trim() !== '') {
+            setTasks([...tasks, newTask]);
+            setNewTask('');
+        }
+    };
+
+    return (
+        <>
+         <input value={newTask} onChange={handleInputChange} placeholder='Ingrese la tarea' type="text"/>
+            <button onClick={handleAddTask}>Crear tarea</button>
+            {tasks.map((task, index) => (
+                <div key={index}>
+                <h2>{task}</h2>
+                </div>
+            ))}
+        </>
+    );
+};
